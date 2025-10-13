11df6bfe01756a7303e7144ffb40055a207d46cc
mostfa-charfedin <akkezmostfacharfedin@gmail.com>
2025-10-13 22:18:12 +0100

    first commit

---
+++ src/main/java/sicam/compltickets_backend/ComplTicketsBackendApplication.java
@@ -0,0 +1,13 @@
+package sicam.compltickets_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComplTicketsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComplTicketsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sicam/compltickets_backend/ComplTicketsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package sicam.compltickets_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComplTicketsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
